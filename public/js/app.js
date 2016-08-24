865209502e92d86ef569dc053c569f2993aa822f
Jacob Paine <painejacob@gmail.com>
2016-08-24 10:55:25 -0500

    Tames directional bug

--- public/js/app.js
+++ public/js/app.js
@@ -11,24 +11,14 @@ angular.module("contactsApp", ['ngRoute'])
             //         }
             //     }
             // })
-            .when("/", {
-              templateUrl: "rooms_list.html",
-              controller: "RoomsController",
-              resolve: {
-                rooms: function(RoomsService) {
-                  console.log("rooms resolve happens");
-                  return RoomsService.getRooms();
-                }
-              }
-            })
-            .when("/new/contact", {
-                controller: "NewContactController",
-                templateUrl: "contact-form.html"
-            })
-            .when("/contact/:contactId", {
-                controller: "EditContactController",
-                templateUrl: "contact.html"
-            })
+            // .when("/new/contact", {
+            //     controller: "NewContactController",
+            //     templateUrl: "contact-form.html"
+            // })
+            // .when("/contact/:contactId", {
+            //     contro ller: "EditContactController",
+            //     templateUrl: "contact.html"
+            // })
             // route for the rooms pages
             .when('/rooms/:roomId', {
               templateUrl : 'rooms/rooms_temp.html',
@@ -66,65 +56,65 @@ angular.module("contactsApp", ['ngRoute'])
               });
         }
     })
-    .service("Contacts", function($http) {
-      console.log("The Contacts service is getting used");
-
-        this.getContacts = function() {
-          console.log("getContacts happens");
-            return $http.get("/contacts").
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error finding contacts.");
-                });
-        }
-        this.createContact = function(contact) {
-          console.log("contact inside service for post", contact);
-            return $http.post("/contacts", contact).
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error creating contact.");
-                });
-        }
-        this.getContact = function(contactId) {
-          console.log("getContact happens");
-            var url = "/contacts/" + contactId;
-            console.log("contactId", contactId);
-            return $http.get(url).
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error finding this contact.");
-                });
-        }
-        this.editContact = function(contact) {
-            var url = "/contacts/" + contact._id;
-            console.log("edit contact id", contact._id);
-            return $http.put(url, contact).
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error editing this contact.");
-                    console.log(response);
-                });
-        }
-        this.deleteContact = function(contactId) {
-            var url = "/contacts/" + contactId;
-            return $http.delete(url).
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error deleting this contact.");
-                    console.log(response);
-                });
-        }
-    })
+    // .service("Contacts", function($http) {
+    //   console.log("The Contacts service is getting used");
+    //
+    //     this.getContacts = function() {
+    //       console.log("getContacts happens");
+    //         return $http.get("/contacts").
+    //             then(function(response) {
+    //                 return response;
+    //             }, function(response) {
+    //                 alert("Error finding contacts.");
+    //             });
+    //     }
+    //     this.createContact = function(contact) {
+    //       console.log("contact inside service for post", contact);
+    //         return $http.post("/contacts", contact).
+    //             then(function(response) {
+    //                 return response;
+    //             }, function(response) {
+    //                 alert("Error creating contact.");
+    //             });
+    //     }
+    //     this.getContact = function(contactId) {
+    //       console.log("getContact happens");
+    //         var url = "/contacts/" + contactId;
+    //         console.log("contactId", contactId);
+    //         return $http.get(url).
+    //             then(function(response) {
+    //                 return response;
+    //             }, function(response) {
+    //                 alert("Error finding this contact.");
+    //             });
+    //     }
+    //     this.editContact = function(contact) {
+    //         var url = "/contacts/" + contact._id;
+    //         console.log("edit contact id", contact._id);
+    //         return $http.put(url, contact).
+    //             then(function(response) {
+    //                 return response;
+    //             }, function(response) {
+    //                 alert("Error editing this contact.");
+    //                 console.log(response);
+    //             });
+    //     }
+    //     this.deleteContact = function(contactId) {
+    //         var url = "/contacts/" + contactId;
+    //         return $http.delete(url).
+    //             then(function(response) {
+    //                 return response;
+    //             }, function(response) {
+    //                 alert("Error deleting this contact.");
+    //                 console.log(response);
+    //             });
+    //     }
+    // })
     .controller("MoveController", function($scope, $routeParams, rooms, $location)  {
+      var roomsData = rooms.data;
+      var room_num = parseInt($routeParams.roomId, 10);
       $scope.room_id = $routeParams.roomId;
       $scope.rooms = rooms.data;
-      var roomsData = rooms.data
-      var room_num = parseInt($routeParams.roomId, 10);
       $scope.room_num = parseInt($routeParams.roomId, 10);
 
       // This controls all available directions in the entire game.
@@ -134,28 +124,29 @@ angular.module("contactsApp", ['ngRoute'])
         var thisRoomNumber = $routeParams.roomId;
         for ( prop in roomsData ) {
           if (roomsData[prop].roomName === "room"+thisRoomNumber){
+
             // Find the directions in the database.
             northValue = roomsData[prop].exit.north;
             southValue = roomsData[prop].exit.south;
             eastValue = roomsData[prop].exit.east;
             westValue = roomsData[prop].exit.west;
-            // Allows for same direction different commands.
-            var northCmd = 'north' && 'n';
-            var southCmd = 'south' && 's';
-            var eastCmd = 'east' && 'e';
-            var westCmd = 'west' && 'w';
 
-            if (text === northCmd && northValue === true){
+            //Alternate keys for same directions
+            var northKey = (text === 'north' || text === 'n');
+            var southKey = (text === 'south' || text === 's');
+            var eastKey = (text === 'east' || text === 'e');
+            var westKey = (text === 'west' || text === 'w');
+
+            if (northKey && northValue === true){
               var newRoom = (room_num + 10).toString();
               $location.path('rooms/' + newRoom);
-            } else if (text === southCmd && southValue === true){
+            } else if (southKey && southValue === true){
               var newRoom = (room_num - 10).toString();
-              console.log("south");
               $location.path('rooms/' + newRoom);
-            } else if (text === eastCmd && eastValue === true){
+            } else if (eastKey && eastValue === true){
               var newRoom = (room_num + 1).toString();
               $location.path('rooms/' + newRoom);
-            } else if (text === westCmd && westValue === true){
+            } else if (westKey && westValue === true){
               var newRoom = (room_num - 1).toString();
               $location.path('rooms/' + newRoom);
             } else {
@@ -167,54 +158,48 @@ angular.module("contactsApp", ['ngRoute'])
       };
     })
 
-    .controller("ListController", function(contacts, $scope) {
-        $scope.contacts = contacts.data;
-    })
-    .controller("NewContactController", function($scope, $location, Contacts) {
-        $scope.back = function() {
-            $location.path("/");
-        }
-
-        $scope.saveContact = function(contact) {
-            Contacts.createContact(contact).then(function(doc) {
-              console.log("contact inside save", contact);
-                var contactUrl = "/contact/" + doc.data._id;
-                $location.path(contactUrl);
-            }, function(response) {
-                alert(response);
-            });
-        }
-    })
-    .controller("EditContactController", function($scope, $routeParams, Contacts) {
-        Contacts.getContact($routeParams.contactId).then(function(doc) {
-            $scope.contact = doc.data;
-        }, function(response) {
-            alert(response);
-        });
-
-        $scope.toggleEdit = function() {
-            $scope.editMode = true;
-            $scope.contactFormUrl = "contact-form.html";
-        }
-
-        $scope.back = function() {
-            $scope.editMode = false;
-            $scope.contactFormUrl = "";
-        }
-
-        $scope.saveContact = function(contact) {
-            Contacts.editContact(contact);
-            $scope.editMode = false;
-            $scope.contactFormUrl = "";
-        }
-
-        $scope.deleteContact = function(contactId) {
-            Contacts.deleteContact(contactId);
-        }
-    })
-    .controller("RoomsController", function(rooms, $scope) {
-        console.log("Rooms Controller online");
-        console.log("rooms param", rooms);
-        $scope.rooms = rooms.data;
-
-    });
+    // .controller("ListController", function(contacts, $scope) {
+    //     $scope.contacts = contacts.data;
+    // })
+    // .controller("NewContactController", function($scope, $location, Contacts) {
+    //     $scope.back = function() {
+    //         $location.path("/");
+    //     }
+    //
+    //     $scope.saveContact = function(contact) {
+    //         Contacts.createContact(contact).then(function(doc) {
+    //           console.log("contact inside save", contact);
+    //             var contactUrl = "/contact/" + doc.data._id;
+    //             $location.path(contactUrl);
+    //         }, function(response) {
+    //             alert(response);
+    //         });
+    //     }
+    // })
+    // .controller("EditContactController", function($scope, $routeParams, Contacts) {
+    //     Contacts.getContact($routeParams.contactId).then(function(doc) {
+    //         $scope.contact = doc.data;
+    //     }, function(response) {
+    //         alert(response);
+    //     });
+    //
+    //     $scope.toggleEdit = function() {
+    //         $scope.editMode = true;
+    //         $scope.contactFormUrl = "contact-form.html";
+    //     }
+    //
+    //     $scope.back = function() {
+    //         $scope.editMode = false;
+    //         $scope.contactFormUrl = "";
+    //     }
+    //
+    //     $scope.saveContact = function(contact) {
+    //         Contacts.editContact(contact);
+    //         $scope.editMode = false;
+    //         $scope.contactFormUrl = "";
+    //     }
+    //
+    //     $scope.deleteContact = function(contactId) {
+    //         Contacts.deleteContact(contactId);
+    //     }
+    // })
